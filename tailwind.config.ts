01c980825aa637e87fc09d788cbfb85bb72dc54d
MAXIQUEEN-OS <maxiqueen2025@gmail.com>
2026-06-14 15:10:59 -0500

    Simplify content paths in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from "tailwindcss";
 export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: { extend: {} },
   plugins: [],
 } satisfies Config;
